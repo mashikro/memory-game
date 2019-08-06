5fc4603ac403845d0053203a05c5e96500db228b
Masha Ikromova <mashikro@gmail.com>
2019-08-06 16:41:50 -0700

    Intro function w/ welcome + inst.

---
+++ typing_game.py
@@ -0,0 +1,22 @@
+#print("Hello world!")
+
+level_1_words = ["jazz", "fury", "keys", "bird", "cute", "pens", "blue"]
+
+def intro():
+    print("Hi there! Welcome to my typing/memory game.")
+    print("🧠 🧠 🧠 🧠 🧠 🧠 🧠 🧠 🧠 🧠")
+    print("Its purpose is to help you work onn your muscle memory and improve your typing accuracy and speed 🤓.")
+    print("*************")
+    print("Instructions: ")
+    print("*************")
+    print("The game will flash you a word for X seconds and then it will disappear. Your task is to type the word you saw as fast and as accurately as possible.") 
+    print("*************")
+    print("If you meet the typing speed and accuracy criteria you will move on to the next level.")
+    print("Typing speed: XX")
+    print("Typing accuracy: XX")
+    print("*************")
+    print("If you don't meet the requirements....👿 👿 👿")
+    print("You are given 5 lives for the entire game. If you loose all 5 lives....GAME OVER!!")
+    print("*************")
+ 
+intro()
