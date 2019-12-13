871f1ae973cc016c76fd74da8a0dbe9b8f4980b9
Masha Ikromova <mashikro@gmail.com>
2019-12-13 11:45:39 -0800

    Set up the server file

---
+++ server.py
@@ -0,0 +1,30 @@
+################# Necessary IMPORTS ############################
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, request, redirect, session, flash, jsonify
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+import os
+ 
+#################### FLASK APP SET-UP ####################################
+app = Flask(__name__)
+
+app.secret_key = os.environ['FLASK_SECRET_KEY']
+
+app.jinja_env.undefined = StrictUndefined #to prevent silent but deadly jinja errors
+
+######################### ROUTES #####################################
+
+
+
+
+
+####################### RUNNING MY SERVER ###############################
+if __name__ == "__main__":
+   
+    app.debug=False # We have to set debug=True here, since it has to be True at the point that we invoke the DebugToolbarExtension
+
+    DebugToolbarExtension(app) # Use the DebugToolbar
+
+    app.run(host="0.0.0.0") 
