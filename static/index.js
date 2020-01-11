11fe40f67a413c6e2630b779516b2c8581970917
Masha Ikromova <mashikro@gmail.com>
2020-01-11 10:33:34 +0800

    broke up functions and created multi level functinality

--- static/index.js
+++ static/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
 let currentWord = null;
-
+let currentLevel = 0;
+//set up
 $(document).ready(() => {
   const playButton = $("#playButton");
   const instrButton = $("#instructionsButton");
@@ -17,33 +18,45 @@ $(document).ready(() => {
   playButton.on("click", () => {
     $("#instructions").hide();
     $("#instructionsButton").hide();
-
-    $.getJSON("/play.json", data => {
-      //write a func to show word
-      console.log("is data coming in", data);
-
-      currentWord = data.word;
-
-      $("#displayed-word").html(data.word);
-
-      $("#displayed-word").show();
-
-      // timer starts after the word comes in from server
-      setTimeout(() => $("#displayed-word").hide(), 3000);
-      setTimeout(() => $("#word-form").show(), 3500);
-    });
+    getWord();
   });
   console.log("Is this working????");
 });
+
+// func makes ajax request to get word
+function getWord() {
+  const levelData = {
+    level: currentLevel
+  };
+
+  $.getJSON("/play.json", levelData, data => {
+    //write a func to show word
+    console.log("is data coming in", data);
+
+    currentWord = data.word;
+
+    $("#displayed-word").html(data.word);
+
+    $("#displayed-word").show();
+
+    // timer starts after the word comes in from server
+    setTimeout(() => $("#displayed-word").hide(), 3000);
+    setTimeout(() => $("#word-form").show(), 3100);
+  });
+}
 
 //write a func to validate user input
 function validateWord() {
   console.log("Are we getting here?");
   if (document.myForm.userInput.value == currentWord) {
     alert("Correct!");
-    // document.myForm.Name.focus() ;
-  } else alert("Incorrect!");
-
+    currentLevel += 1;
+    // getWord();
+  } else {
+    // getWord();
+    alert("Incorrect!");
+  }
+  getWord();
   return false;
 }
 
